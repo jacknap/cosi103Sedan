2735eef631361a4929aa4ec52f2fc4f7ed5a4514
kcheerio <kenkirio@brandeis.edu>
2023-03-26 18:51:34 -0400

    edit comments to match assignment

--- pa03/tracker.py
+++ pa03/tracker.py
@@ -4,21 +4,24 @@ import sys
 from transaction import Transaction
 
 
-# here are some helper functions ...
-
 def print_usage():
     ''' print an explanation of how to use this command '''
     print('''Enter a number to execute that command:
             0. quit
             1. show transactions
             2. add transaction
-                Usage: \"2 item_no amount category YYYY-MM-DD desc\"
+                Usage: \"2 item_no amount category YYYY-MM-DD description\"
             3. delete transaction
                 Usage: \"3 item_no\"
             4. delete all transactions
             5. summarize transactions by date
+                Usage: \"5 YYYY-MM-DD\" for items from the given date
+                    or \"5\" for all items sorted by date
             6. summarize transactions by month
+                Usage: \"6 MM\"
+                    or \"6 YYYY-MM\"
             7. summarize transactions by year
+                Usage: \"7 YYYY\"
             8. summarize transactions by category
                 Usage: \"8 category\"
             9. print this menu
@@ -26,16 +29,15 @@ def print_usage():
           )
 
 
-def print_transactions(todos):
-    ''' print the todo items '''
-    if len(todos) == 0:
+def print_transactions(transactions):
+    ''' print the transaction items '''
+    if len(transactions) == 0:
         print('no transactions to print')
         return
-    print('\n')
-    print("%-10s %-10s %-10s %-15s %-30s" %
-          ('item #', 'amount', 'category', 'date', 'desc'))
+    print("\n%-10s %-10s %-10s %-15s %-30s" %
+          ('item #', 'amount', 'category', 'date', 'description'))
     print('-'*60)
-    for item in todos:
+    for item in transactions:
         values = tuple(item.values())  # (rowid,title,desc,completed)
         # print(values)
 
@@ -48,46 +50,70 @@ def process_args(arglist):
 
     if arglist[0] == "1":
         print_transactions(trans.show())
+
     elif arglist[0] == '2':
         if len(arglist) != 6:
             print(
-                "Incorrect syntax\nUsage: \"2 item_no amount category YYYY-MM-DD desc\"\n")
+                "Incorrect syntax\nUsage: \"2 item_no amount category YYYY-MM-DD description\"\n")
         else:
             todo = {'item_no': arglist[1], 'amount': arglist[2],
                     'category': arglist[3], 'date': arglist[4], 'desc': arglist[5]}
             trans.add(todo)
+
     elif arglist[0] == '3':
         if len(arglist) != 2:
             print("Incorrect syntax\nUsage: \"3 item_no\"\n")
         else:
             trans.delete(arglist[1])
+
     elif arglist[0] == '4':
         confirm = input(
-            "Are you sure you want to delete all transactions? (y/[n])\n> ")
+            "Are you sure you want to delete all transactions? (y/n)\n> ")
         if confirm in ('y', 'Y'):
             trans.clear()
         else:
             print("Did not delete all transactions\n")
+
     elif arglist[0] == '5':
-        print("Transactions sorted by date: ")
-        print_transactions(trans.sumbydate())
-    elif arglist[0] == '6':
-        if len(arglist) != 2:
-            print("Incorrect syntax\nUsage: \"6 MM\"\n")
+        if len(arglist) == 1:
+            print("Transactions sorted by date: ")
+            print_transactions(trans.sort_date())
+        elif len(arglist) == 2:
+            print("Transactions from ", arglist[1], ": ", sep='')
+            print_transactions(trans.summary_date(arglist[1]))
         else:
-            print("Transactions sorted by month: ")
-            print_transactions(trans.sumbymonth(arglist[1]))
+            print("Incorrect syntax\nUsage: \"5 YYYY-MM-DD\" or \"5\"\n")
+
+    elif arglist[0] == '6':
+        if len(arglist) == 2:
+            if len(arglist[1].split('-')) == 1:
+                print("Transactions from month ", arglist[1], ": ", sep='')
+                print_transactions(trans.summary_month(arglist[1]))
+            elif len(arglist[1].split('-')) == 2:
+                print("Transactions from ", arglist[1], ": ", sep='')
+                print_transactions(trans.summary_month_year(arglist[1]))
+            else:
+                print("Incorrect syntax\nUsage: \"6 MM\" or \"6 YYYY-MM\"\n")
+        else:
+            print("Incorrect syntax\nUsage: \"6 MM\" or \"6 YYYY-MM\"\n")
+
     elif arglist[0] == '7':
-        print("Transactions sorted by year: ")
-        print_transactions(trans.sumbyyear())
+        if len(arglist) != 2:
+            print("Incorrect syntax\nUsage: \"7 YYYY\"\n")
+        else:
+            print("Transactions from ", arglist[1], ": ", sep='')
+            print_transactions(trans.summary_year(arglist[1]))
+
     elif arglist[0] == '8':
         if len(arglist) != 2:
             print("Incorrect syntax\nUsage: \"8 category\"\n")
         else:
-            print("Transactions sorted by category: ")
-            print_transactions(trans.sumbycategory(arglist[1]))
+            print("Transactions from the ", arglist[1], " category: ", sep='')
+            print_transactions(trans.summary_category(arglist[1]))
+
     elif arglist[0] == "9":
         print_usage()
+
     else:
         print(arglist[0], "is not implemented\n")
 
--- pa03/transaction.py
+++ pa03/transaction.py
@@ -4,7 +4,7 @@ import sqlite3
 
 
 def to_dict(tpl):
-    ''' t is a tuple (rowid,title, desc,completed)'''
+    ''' t is a tuple (item_no,amount,category,date, desc)'''
     todo = {'item_no': tpl[0], 'amount': tpl[1],
             'category': tpl[2], 'date': tpl[3], 'desc': tpl[4]}
     return todo
@@ -14,6 +14,8 @@ class Transaction():
     ''' a class to make SQL queries to a database of transactions '''
 
     def __init__(self, dbfile):
+        '''Constructor for Transaction. dbfile is file name of the database '''
+        self.dbfile = dbfile
         con = sqlite3.connect(dbfile)
         con.execute('''CREATE TABLE IF NOT EXISTS transactions
                     (item_no INT, 
@@ -22,52 +24,52 @@ class Transaction():
                     date DATE,
                     desc VARCHAR(255))''')
         con.commit()
-        self.run_query('''CREATE TABLE IF NOT EXISTS transactions
-                    (item_no INT, 
-                    amount INT,
-                    category VARCHAR(255),
-                    date DATE,
-                    desc VARCHAR(255))''',
-                       ())
 
     def show(self):
-        ''' return all of the transaction as a list of dicts '''
+        ''' return all of the transactions as a list of dicts '''
         return self.run_query("SELECT * from transactions", ())
 
     def add(self, item):
-        ''' create a transaction item and add it to the todo table '''
+        ''' create a transaction item and add it to the transaction table '''
         return self.run_query("INSERT INTO transactions VALUES(?,?,?,?,?)",
                               (item['item_no'], item['amount'], item['category'],
                                item['date'], item['desc']))
 
     def clear(self):
-        ''' delete all of the transaction '''
+        ''' delete all of the transactions '''
         return self.run_query("DELETE from transactions", ())
 
     def delete(self, item_no):
         ''' delete a transaction item '''
         return self.run_query("DELETE FROM transactions WHERE item_no=?", (item_no,))
 
-    def sumbydate(self):
-        ''' return transaction sorted by date '''
+    def sort_date(self):
+        ''' return transaction(s) sorted by date '''
         return self.run_query('SELECT * from transactions ORDER BY date', ())
 
-    def sumbymonth(self, month):
-        ''' return transaction sorted by month '''
+    def summary_date(self, date):
+        ''' return transaction(s) from a given date '''
+        return self.run_query("SELECT * FROM transactions WHERE strftime('%Y-%m-%d', date)=?", (date,))
+
+    def summary_month(self, month):
+        ''' return transaction(s) from a given month '''
         return self.run_query("SELECT * FROM transactions WHERE strftime('%m', date)=?", (month,))
 
-    def sumbyyear(self):
-        ''' return transaction sorted by year '''
-        # working, but not correctly
-        return self.run_query('SELECT * from transactions ORDER BY date', ())
+    def summary_month_year(self, year_month):
+        ''' return transaction(s) from a given month and year '''
+        return self.run_query("SELECT * FROM transactions WHERE strftime('%Y-%m', date)=?", (year_month,))
 
-    def sumbycategory(self, category):
-        ''' return transaction sorted by category '''
+    def summary_year(self, year):
+        ''' return transaction(s) from a given year '''
+        return self.run_query("SELECT * FROM transactions WHERE strftime('%Y', date)=?", (year,))
+
+    def summary_category(self, category):
+        ''' return transaction(s) from a given category '''
         return self.run_query("SELECT * from transactions WHERE category=?", (category,))
 
     def run_query(self, query, tpl):
-        ''' return all of the uncompleted tasks as a list of dicts.'''
-        con = sqlite3.connect('transactions.db')
+        ''' return all of the transactions as a list of dicts.'''
+        con = sqlite3.connect(self.dbfile)
         cur = con.cursor()
         cur.execute(query, tpl)
         tuples = cur.fetchall()
