b8d08e01e6ef64a6284b04208bb25a4475fb19b5
kcheerio <kenkirio@brandeis.edu>
2023-05-01 19:02:29 -0400

    ken's pages

---
+++ ca02/routes/ken.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const GptQuery = require('../models/GptQuery')
+
+const { Configuration, OpenAIApi } = require("openai");
+const apiKey = "sk-XXXXX" // Set your OpenAI API key here
+const configuration = new Configuration({ apiKey: apiKey });
+const openai = new OpenAIApi(configuration);
+
+
+router.get('/index/ken', (req,res,next) => {
+    res.render('kenBio');
+});
+
+router.get('/gpt/ken', (req,res,next) => {
+    res.render('kenInput');
+});
+
+router.post('/gpt/ken', async (req,res,next) => {
+    res.locals.prompt = req.body.query;
+	const gptQuery = new GptQuery(
+		{query: req.body.query,
+		date: req.body.date,
+		userId: req.user._id
+		})
+	await gptQuery.save();
+    res.locals.response = await askGpt(req.body.query);
+    res.render('kenResponse');
+});
+
+const askGpt = async (query) => {
+	let completion = openai.createCompletion({
+		model: "text-davinci-003",
+		prompt: "Please convert the following code from Python into Java: "+query,
+		max_tokens: 1024,
+		n: 1,
+		temperature: 0.8,
+	});
+	let response = await completion;
+	return response.data.choices[0].text;
+}
+
+module.exports = router;
